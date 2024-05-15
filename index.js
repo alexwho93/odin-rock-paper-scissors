9749cb7e10fedd2272ccf9c29397ebb3c947f945
alexwho93 <25087931+alexwho93@users.noreply.github.com>
2024-05-15 09:26:28 +0000

    Adding UI

--- index.js
+++ index.js
@@ -3,59 +3,107 @@ function camelize(word) {
 }
 
 function getComputerChoice() {
-  const hand = ["rock", "paper", "scissors"];
+  const hand = ["rock", "paper", "scissor"];
   const randomNumber = Math.floor(Math.random() * 3);
 
   return hand[randomNumber];
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt("Your choice?\nrock, paper or scissors");
+function getEmoji(word) {
+  let result = "";
+  switch (word) {
+    case "rock":
+      result = "👊";
+      break;
+    case "paper":
+      result = "✋";
+      break;
+    case "scissor":
+      result = "✌️";
+      break;
+    default:
+      alert("Error! Please refresh");
+  }
 
-  return humanChoice.toLowerCase();
+  return result;
 }
 
 function playGame() {
+  const rockButton = document.querySelector(".rock");
+  const paperButton = document.querySelector(".paper");
+  const scissorButton = document.querySelector(".scissor");
+  rockButton.addEventListener("click", () =>
+    playRound("rock", getComputerChoice())
+  );
+
+  paperButton.addEventListener("click", () =>
+    playRound("paper", getComputerChoice())
+  );
+
+  scissorButton.addEventListener("click", () =>
+    playRound("scissor", getComputerChoice())
+  );
+
+  const roundNode = document.querySelector(".round");
+  const winStatus = document.querySelector(".win-status");
+  const winReason = document.querySelector(".win-reason");
+
+  const playerScoreNode = document.querySelector("#player-score");
+  const computerScoreNode = document.querySelector("#computer-score");
+
+  const playerChoiceNode = document.querySelector("#player-choice");
+  const compueterChoiceNode = document.querySelector("#computer-choice");
+
   let humanScore = 0;
   let computerScore = 0;
+  let round = 0;
 
   function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice === "scissors") {
+    if (humanChoice === "rock" && computerChoice === "scissor") {
       humanScore++;
-      console.log(`You win! Rock beats Scissors`);
+
+      winStatus.textContent = "You win!";
+      winReason.textContent = "Rock beats Scissor!";
     } else if (humanChoice === "paper" && computerChoice === "rock") {
       humanScore++;
-      console.log(`You win! Paper beats Rock`);
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+
+      winStatus.textContent = "You win!";
+      winReason.textContent = "Paper beats Rock!";
+    } else if (humanChoice === "scissor" && computerChoice === "paper") {
       humanScore++;
-      console.log(`You win! Scissors beats Paper`);
+
+      winStatus.textContent = "You win!";
+      winReason.textContent = "Scissor beats Paper!";
     } else if (humanChoice === computerChoice) {
-      console.log(`It's a draw!`);
+      winStatus.textContent = "It's a draw!";
+      winReason.textContent = "";
     } else {
-      console.log(
-        `You lose! ${camelize(computerChoice)} beats ${camelize(humanChoice)}`
-      );
+      winStatus.textContent = "You lose!";
+      winReason.textContent = `${camelize(computerChoice)} beats ${camelize(
+        humanChoice
+      )}`;
       computerScore++;
     }
-  }
+    round++;
 
-  for (let round = 1; round <= 5; round++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+    if (humanScore >= 5) {
+      playerScoreNode.textContent = "5";
+      winStatus.textContent = "Congratulation! You won the game!";
+      winReason.textContent = "";
+      return;
+    } else if (computerScore >= 5) {
+      computerScoreNode.textContent = "5";
+      winStatus.textContent = "Sorry, you lost the game!";
+      winReason.textContent = "";
+      return;
+    }
 
-    console.log(
-      `Round: ${round} / humanScore: ${humanScore} computerScore: ${computerScore}`
-    );
+    roundNode.textContent = "Round: " + round.toString();
+    playerScoreNode.textContent = humanScore.toString();
+    computerScoreNode.textContent = computerScore.toString();
 
-    playRound(humanSelection, computerSelection);
-  }
-
-  if (humanScore > computerScore) {
-    console.log("Congratulations you win the game!");
-  } else if (humanScore === computerScore) {
-    console.log("It's a draw!");
-  } else {
-    console.log("You lose the game!");
+    playerChoiceNode.textContent = getEmoji(humanChoice);
+    compueterChoiceNode.textContent = getEmoji(computerChoice);
   }
 }
 
